8a268da87ae91118eb03901c5fc9977155824455
Drew Dolgert <adolgert@uw.edu>
2023-11-20 10:40:38 -0500

    capitalize the name and make an ArrivalServer that is exponentially-arriving jobs

--- src/server.jl
+++ src/server.jl
@@ -10,28 +10,29 @@ using Random
 # A server can send jobs to multiple queues.
 # Multiple servers can get jobs from the same queue.
 
-export Server
+abstract type Server end
 
+export Server, ArrivalServer, ModifyServer
 
-mutable struct Server
+mutable struct ModifyServer <: Server
     rate::UnivariateDistribution
     modify_token::Function
     disbursement::Disbursement
     id::Int
 end
 
-function Server(rate::Float64)
-    Server(Exponential(rate), identity, RoundRobin(), zero(Int))
+function ModifyServer(rate::Float64)
+    ModifyServer(Exponential(1.0 / rate), identity, RoundRobin(), zero(Int))
 end
 
-id!(s::Server, id::Int) = (s.id = id; s)
-id(s::Server) = s.id
+id!(s::ModifyServer, id::Int) = (s.id = id; s)
+id(s::ModifyServer) = s.id
 
-rate(s::Server, token) = s.rate
-modify!(s::Server, token, when) = (s.modify_token(token); nothing)
+rate(s::ModifyServer, token) = s.rate
+modify!(s::ModifyServer, token, when) = (s.modify_token(token); nothing)
 
 
-function update_downstream!(s::Server, downstream, when, rng)
+function update_downstream!(s::ModifyServer, downstream, when, rng)
     update_downstream!(s.disbursement, downstream, when, rng)
 end
 
@@ -41,14 +42,14 @@ If you need an arrival rate of tokens, make an InfiniteQueue of tokens
 and feed it to this ArrivalServer. This will create tokens at the needed
 rate.
 """
-mutable struct ArrivalServer
+mutable struct ArrivalServer <: Server
     rate::UnivariateDistribution
     disbursement::Disbursement
     id::Int
 end
 
 function ArrivalServer(rate::Float64)
-    ArrivalServer(Exponential(rate), identity, RoundRobin(), zero(Int))
+    ArrivalServer(Exponential(1.0 / rate), RoundRobin(), zero(Int))
 end
 
 id!(s::ArrivalServer, id::Int) = (s.id = id; s)
