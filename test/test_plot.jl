a828f24e79accc9c5c3aaf445b6cd0b8dcc8b79f
Drew Dolgert <adolgert@uw.edu>
2023-11-26 12:13:38 -0500

    can now read from an inkscape file to make a model

--- test/test_plot.jl
+++ test/test_plot.jl
@@ -1,4 +1,5 @@
 using GraphPlot, Compose, Colors
+using EzXML
 import Cairo, Fontconfig
 
 
@@ -41,4 +42,51 @@ import Cairo, Fontconfig
     nodecolor = [colorant"lightseagreen", colorant"orange"]
     p = gplot(graph, nodelabel=labels, nodefillc=nodecolor[membership])
     draw(PNG("plot_start.png", 16cm, 16cm), p)
+end
+
+
+@testset "Read from Inkscape" begin
+    # This test shows how to make a drawing of a network of queues
+    # and read it into the package. Inkscape saves as SVG with annotations.
+    # If you make a drawing, right-click on each queue and server,
+    # open the object properties, and set the label of queues to "queue"
+    # and of servers to "server". Then this can read the SVG for the
+    # network and names.
+    doc = parsexml(read(open("test/queue2.svg", "r")))
+    r = root(doc)
+    graph = nothing
+    for top in eachelement(r)
+        if nodename(top) == "g"
+            graph = top
+        end
+    end
+    q_id = Vector{String}()
+    s_id = Vector{String}()
+    connect = Vector{Tuple{String,String}}()
+    names = Dict{String,String}()
+
+    tag_match = r"url\(\#([a-z0-9\-]+)\)"
+    for child in eachelement(graph)
+        if haskey(child, "inkscape:label") && child["inkscape:label"] == "queue"
+            push!(q_id, child["id"])
+        elseif haskey(child, "inkscape:label") && child["inkscape:label"] == "server"
+            push!(s_id, child["id"])
+        elseif nodename(child) == "path"
+            # Strip the leading hash.
+            source = child["inkscape:connection-start"][2:end]
+            target = child["inkscape:connection-end"][2:end]
+            push!(connect, (source, target))
+        elseif nodename(child) == "text"
+            style = child["style"]
+            m = match(tag_match, style)
+            if m !== nothing
+                target = m.captures[1]
+                names[target] = nodecontent(child)
+            end
+        end
+    end
+    @test length(q_id) == 3
+    @test length(s_id) == 2
+    @test length(connect) == 4
+    @test length(names) == 5
 end
