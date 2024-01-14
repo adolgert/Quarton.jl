f3e6d5bf1a07b7a84b45090dac89e0014a27b0cc
slwu89 <10673535+slwu89@users.noreply.github.com>
2024-01-14 14:48:53 -0800

    fix order of edge morphisms

--- src/cats/model-cats.jl
+++ src/cats/model-cats.jl
@@ -20,6 +20,8 @@ using Catlab
 
 # our model is a "decorated bipartite graph"
 # let V₁ be servers, V₂ be queues
+# E₁₂ is edges from servers to queues
+# E₂₁ is edges from queues to servers
 # src₁,tgt₁ are for edges from servers to queues, v.v. for subscript 2
 @present QueueModelSch <: SchBipartiteGraph begin
 
@@ -46,6 +48,7 @@ end
 
 @acset_type QueueModelType(QueueModelSch, index=[:src₁,:tgt₁,:src₂,:tgt₁]) <: AbstractBipartiteGraph 
 
+# look at the schema
 # to_graphviz(QueueModelSch)
 
 TokenType = Work
@@ -72,17 +75,59 @@ add_part!(
 )
 
 add_part!(
-    model, :E₁₂,
-    src₁ = only(incident(model, :source, :queue_name)),
-    tgt₁ = only(incident(model, :s1, :server_name)),
-    queue_role = :only
-)
-
-add_part!(
     model, :E₂₁,
-    src₂ = only(incident(model, :s1, :server_name)),
-    tgt₂ = only(incident(model, :sink, :queue_name)),
+    src₂ = only(incident(model, :source, :queue_name)),
+    tgt₁ = only(incident(model, :s1, :server_name)),
     server_role = :only
 )
 
+add_part!(
+    model, :E₁₂,
+    src₁ = only(incident(model, :s1, :server_name)),
+    tgt₂ = only(incident(model, :sink, :queue_name)),
+    queue_role = :only
+)
+
 to_graphviz(model, node_labels=(:server_name,:queue_name), edge_labels=(:queue_role,:server_role))
+
+trajectory = Trajectory(2342334)
+start_time = zero(Float64)
+
+# activate!(model, trajectory, s1, T())
+
+# for i in 1:100
+#     when, which = next(trajectory)
+#     @test isfinite(when)
+#     step!(model, trajectory, (when, which))
+# end
+
+s_id = only(incident(model, :s1, :server_name))
+token = TokenType()
+
+model[s_id,:server_tokens] = [token]
+
+Quarton.start!(trajectory, s_id, Quarton.rate(model[s_id,:server], token))
+
+model[s_id,:server_available] = false
+
+# the sim loop
+when, which = next(trajectory)
+
+# function step!(model::T, trajectory, (when, s_event_id)) where {T<:QueueModelType}
+    s_event_id = which
+    Quarton.stop!(trajectory, s_event_id, when)
+    s_event = model[s_event_id, :server]
+
+# end
+
+# lots of dispatch and moving between methods in different files from here on out
+# (4th line onward in the step! method)
+
+# queues downstream of this server
+q_dest = model[incident(model, s_event_id, :src₁), [:tgt₂,:queue]]
+
+r = s_event.disbursement
+
+# push!(downstream, q_dest[r.next], when)
+event_token = only(model[s_event_id, :server_tokens])
+s_event.modify_token(event_token)
