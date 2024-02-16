0af4961c11d9f6056bcf6a3935c6b14e3fde9e45
Drew Dolgert <adolgert@uw.edu>
2024-02-16 15:47:22 -0500

    moved acset into the project

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,18 @@
 using Test
 using Quarton
 
-@testset "server" begin
-    include("test_server.jl")
-end
+# @testset "server" begin
+#     include("test_server.jl")
+# end
 
-@testset "model" begin
-    include("test_model.jl")
-end
+# @testset "model" begin
+#     include("test_model.jl")
+# end
 
-@testset "machine" begin
-    include("test_machine.jl")
-end
+# @testset "machine" begin
+#     include("test_machine.jl")
+# end
 
-@testset "plot" begin
-    include("test_plot.jl")
-end
+# @testset "plot" begin
+#     include("test_plot.jl")
+# end
